a6636b637c8f549ece472def7f7848cb2431b2dd
Yiz <68924783+TheCodeKat@users.noreply.github.com>
2021-09-13 13:08:05 -0500

    Updated unit tests

--- src/app/current-weather/current-weather.component.spec.ts
+++ src/app/current-weather/current-weather.component.spec.ts
@@ -1,10 +1,9 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing'
-import { By } from '@angular/platform-browser'
-import { injectSpy } from 'angular-unit-test-helper'
+import { autoSpyObj, injectSpy, ObservablePropertyStrategy } from 'angular-unit-test-helper'
 import { of } from 'rxjs'
+import { MaterialModule } from '../material.module'
 
 import { WeatherService } from '../weather/weather.service'
-import { fakeWeather } from '../weather/weather.service.fake'
 import { CurrentWeatherComponent } from './current-weather.component'
 
 describe('CurrentWeatherComponent', () => {
@@ -14,17 +13,18 @@ describe('CurrentWeatherComponent', () => {
 
   beforeEach(
     waitForAsync(() => {
-      const weatherServiceSpy = jasmine.createSpyObj('WeatherService', [
-        'getCurrentWeather',
-      ])
+      const weatherServiceSpy = autoSpyObj(
+        WeatherService,
+        ['currentWeather$'],
+        ObservablePropertyStrategy.BehaviorSubject
+      )
 
       TestBed.configureTestingModule({
         declarations: [CurrentWeatherComponent],
-        // imports: [HttpClientTestingModule], // If we have to include HttpClientTestingModule then we're not really writing a unit test, because CurrentWeatherComponent shouldn't know about HttpClient
+        imports: [MaterialModule],
         providers: [{ provide: WeatherService, useValue: weatherServiceSpy }],
       }).compileComponents()
 
-      // weatherServiceMock = TestBed.inject(WeatherService) as any
       weatherServiceMock = injectSpy(WeatherService)
     })
   )
@@ -43,37 +43,5 @@ describe('CurrentWeatherComponent', () => {
 
     // Assert
     expect(component).toBeTruthy()
-  })
-
-  it('should get currentWeather from weatherService', () => {
-    // Arrange
-    weatherServiceMock.getCurrentWeather.and.returnValue(of())
-
-    // Act
-    fixture.detectChanges() // triggers ngOnInit()
-
-    // Assert
-    expect(weatherServiceMock.getCurrentWeather).toHaveBeenCalledTimes(1)
-  })
-
-  it('should eagerly load currentWeather in Bethesda from weatherService', () => {
-    // Arrange
-    weatherServiceMock.getCurrentWeather.and.returnValue(of(fakeWeather))
-
-    // Act
-    fixture.detectChanges() // triggers ngOnInit()
-
-    // Assert
-    expect(component.current).toBeDefined()
-    if(component.current){
-      expect(component.current.city).toEqual('Bethesda')
-      expect(component.current.temperature).toEqual(280.32)
-    }
-    
-
-    // Assert on DOM
-    //const debugEl = fixture.debugElement
-    //const titleEl: HTMLElement = debugEl.query(By.css('span')).nativeElement
-    //expect(titleEl.textContent).toContain('Bethesda')
   })
 })
